b66d9b0b17d619ce9400a92d6ff342d80466e4c2
Ikumapayi Khalilullah Olaniyi <ikumapayiolaniyi007@gmail.com>
2022-09-14 10:11:48 +0100

    refactor: modify app.js codes

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
+//Database connection import
+const connectDB = require("./db/connect");
+
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
-//Database connection import
-const connectDB = require("./db/connect");
-
-//task route
+//task routes
 const tasks = require("./routes/tasks");
 
-//404 route page
+//404 route and error handlers
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, "./public")));
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
-app.use(notFound)
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async (url) => {
   try {
